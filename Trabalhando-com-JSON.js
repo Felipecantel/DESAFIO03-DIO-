7c5e67b8202aad7e71f955d5b8e6dd4cc0fc7b2d
Felipecantel <felipe.mirandapontes@gmail.com>
2023-11-04 13:02:48 -0300

    first commit

---
+++ Trabalhando-com-JSON.js
@@ -0,0 +1,34 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque = "";
+
+        switch (this.tipo) {
+            case "mago":
+                ataque = " magia";
+                break;
+            case "guerreiro":
+                ataque = " espada";
+                break;
+            case "monge":
+                ataque = " artes marciais";
+                break;
+            case "ninja":
+                ataque = " shuriken";
+                break;
+            default:
+                ataque = " um ataque desconhecido";
+                break;
+        }
+
+        console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+const heroi = new Heroi("Exemplo", 30, "guerreiro");
+heroi.atacar();
